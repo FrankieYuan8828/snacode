2a452a7b4fd1ea072d2c0cf5c8c262f1164f95a2
FrankieYuan8828 <frankieyuan.yyd@gmail.com>
2026-07-28 13:35:15 +0800

    Delete test-sharp.js

--- test-sharp.js
+++
@@ -1,4 +0,0 @@
-const sharp = require('sharp');
-console.log('Sharp loaded successfully');
-console.log('Formats:', Object.keys(sharp.format));
-console.log('SVG support:', sharp.format.svg ? 'available' : 'not available');
